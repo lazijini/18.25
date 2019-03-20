da0a4c2f9aaada85ab5d69492c5d0251b510dc54
lazijini <48761863+lazijini@users.noreply.github.com>
2019-03-20 10:08:27 -0400

    Add files via upload

---
+++ StringPerm.java
@@ -0,0 +1,26 @@
+public class StringPerm
+{
+    public static void main(String[] args)
+    {
+        String s = "abc";
+        displayPermutation(s);
+    }
+    
+    public static void displayPermutation(String s)
+    {
+        displayPermutation("", s);
+    }
+    
+    public static void displayPermutation(String s1, String s2)
+    {
+        if(s2.length() > 0)
+        {
+            System.out.println(s1 + s2);
+            for(int i = 0; i < s2.length(); i++)
+            {
+                s2 = s2.substring(0, i) + s2.substring(i + 1);
+                displayPermutation(s1 + s2.charAt(i), s2);
+            }
+        }
+    }
+}
