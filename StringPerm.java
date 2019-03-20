269b21152613df3c8c0b8cc4408135191a4052fa
lazijini <48761863+lazijini@users.noreply.github.com>
2019-03-20 10:36:24 -0400

    Update StringPerm.java

--- StringPerm.java
+++ StringPerm.java
@@ -1,8 +1,11 @@
+import java.util.Scanner;
+
 public class StringPerm
 {
     public static void main(String[] args)
     {
-        String s = "abc";
+        Scanner input = new Scanner(System.in);
+        String s = input.nextLine();
         displayPermutation(s);
     }
     
@@ -13,16 +16,20 @@ public class StringPerm
     
     public static void displayPermutation(String s1, String s2)
     {
-        String s1Start = s1;
+        String s1SEdit = s1;
+        String s2SEdit = s2;
         if(s2.length() > 0)
         {
-            System.out.println(s1 + s2);
             for(int i = 0; i < s2.length(); i++)
             {
-                s1 = s1Start + s2.charAt(i);
-                s2 = s2.substring(0, i) + s2.substring(i + 1);
-                displayPermutation(s1, s2);
+                s1SEdit = s1 + s2.charAt(i);
+                s2SEdit = s2.substring(0, i) + s2.substring(i + 1);
+                displayPermutation(s1SEdit, s2SEdit);
             }
         }
+        else
+        {
+            System.out.println(s1 + s2);
+        }    
     }
 }
