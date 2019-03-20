0c18a1d10ce426394737a2ea07935af5a5132382
lazijini <48761863+lazijini@users.noreply.github.com>
2019-03-20 10:12:30 -0400

    Update StringPerm.java

--- StringPerm.java
+++ StringPerm.java
@@ -13,13 +13,15 @@ public class StringPerm
     
     public static void displayPermutation(String s1, String s2)
     {
+        String s1Start = s1;
         if(s2.length() > 0)
         {
             System.out.println(s1 + s2);
             for(int i = 0; i < s2.length(); i++)
             {
+                s1 = s1Start + s2.charAt(i);
                 s2 = s2.substring(0, i) + s2.substring(i + 1);
-                displayPermutation(s1 + s2.charAt(i), s2);
+                displayPermutation(s1, s2);
             }
         }
     }
